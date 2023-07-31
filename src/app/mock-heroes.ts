3a64bfe1c141b02b7040f64029dca1e8e82fb9fe
Abhay Vishwakarma <abhay.vishwakarma@neosoftmail.com>
2023-07-31 20:32:52 +0530

    Learned displaying List on frontend

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+export const HEROES : Hero[] = [
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'Batman' },
+  { id: 3, name: 'Hulk' },
+  { id: 4, name: 'Hawk eye' },
+  { id: 5, name: 'RubberMan' },
+  { id: 6, name: 'Dynama' },
+  { id: 7, name: 'Dr. IQ' },
+  { id: 8, name: 'Magma' },
+  { id: 9, name: 'Tornado' }
+];
